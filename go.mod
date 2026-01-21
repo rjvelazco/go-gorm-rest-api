5dbaadf5fae0d4f509a6d31390ebda3324ee7607
Rafael Velazco <rjvelazco21@gmail.com>
2026-01-20 21:10:26 -0400

    Add Gorilla Mux for routing and update Go module dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rjvelazco/go-gorm-restapi
 
 go 1.25.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(":3000", router)
 }
