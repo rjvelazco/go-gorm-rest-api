e2a3bda455aa875070a3a4725bb8670f9227ed30
Rafael Velazco <rjvelazco21@gmail.com>
2026-01-22 16:58:14 -0400

    chore: add Users API Methods

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -4,43 +4,153 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/rjvelazco/go-gorm-restapi/db"
 	"github.com/rjvelazco/go-gorm-restapi/models"
 )
 
+var userNotFound = map[string]interface{}{
+	"message": "User not found",
+	"status":  http.StatusNotFound,
+}
+
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	var users []models.User
 
-	db.DB.Find(&users)
+	result := db.DB.Find(&users)
+	if result.Error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": result.Error.Error()})
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&users)
 }
 
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	var params = mux.Vars(r)
+	var id = params["id"]
 
-	w.Write([]byte("Getting user..."))
+	result := db.DB.First(&user, id)
+	if result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": result.Error.Error()})
+		return
+	}
+
+	if user.ID == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(userNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&user)
 }
 
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 
-	json.NewDecoder(r.Body).Decode(&user)
-
-	createdUser := db.DB.Create(&user)
-	err := createdUser.Error
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Error creating user: " + err.Error()))
+		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON: " + err.Error()})
 		return
 	}
 
+	result := db.DB.Create(&user)
+	if result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Error creating user: " + result.Error.Error()})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(&user)
 }
 
 func UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Updating user..."))
+	var user models.User
+	params := mux.Vars(r)
+
+	result := db.DB.First(&user, params["id"])
+	if result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": result.Error.Error()})
+		return
+	}
+
+	if user.ID == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(userNotFound)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON: " + err.Error()})
+		return
+	}
+
+	if err := db.DB.Save(&user).Error; err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Error updating user: " + err.Error()})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&user)
 }
 
 func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Deleting user..."))
+	var user models.User
+	params := mux.Vars(r)
+
+	result := db.DB.First(&user, params["id"])
+	if result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": result.Error.Error()})
+		return
+	}
+
+	if user.ID == 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(userNotFound)
+		return
+	}
+
+	// This will delete the user, it will set the deleted_at field to the current time
+	// db.DB.Unscoped().Delete(&user)
+
+	// This will not delete the user, it will set the deleted_at field to the current time
+	if err := db.DB.Delete(&user).Error; err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Error deleting user: " + err.Error()})
+		return
+	}
+
+	var userDeleted = map[string]interface{}{
+		"message": "User deleted with id: " + params["id"],
+		"status":  http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&userDeleted)
 }
